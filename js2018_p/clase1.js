b307f47d96126070016348836ee27612eb1c763c
Juan Fernando Ordóñez Arroyave <juanfeorar@hotmail.com>
2018-10-30 10:36:54 -0500

    Clase 1 y 2 de JS platzi

---
+++ js2018_p/clase1.js
@@ -0,0 +1,12 @@
+var nombre;
+
+nombre = 'Juan';
+
+var apellido = 'Ordóñez', edad = '37'
+
+console.log('Hola '+nombre+' '+apellido);
+console.log('Tengo '+edad+' años');
+
+
+var peso = 75;
+
---
+++ js2018_p/clase2.js
@@ -0,0 +1,18 @@
+var nombre = 'Juan', apellido = 'Ordóñez'
+
+var nombreEnMayuscula = nombre.toUpperCase();
+
+var apellidoEnMinusculas = apellido.toLocaleLowerCase();
+
+var primeraLetraDelNombre = nombre.charAt(0);
+var cantidadDeLetrasDelNombre = nombre.length;
+var nombreCompleto = nombre+' '+apellido;
+
+var nombreCompleto1 = `${nombre} ${apellido.toUpperCase()}`;
+
+var str = nombre.charAt(1) + nombre.charAt(2);
+var str1 = nombre.substr(1, 2);
+
+//desafio
+var tamanoNombre = nombre.length;
+var ultimaLetraNombre = nombre.charAt(tamanoNombre-1);
